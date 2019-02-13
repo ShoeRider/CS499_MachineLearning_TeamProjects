cf05a751e3d03f489f07ef7155944c49dc1cfce4
ShoeRider <as3379@nau.edu>
2019-02-12 19:12:26 -0700

    Created file listing of all libraries used to keep a easy way to download and include commands for R

---
+++ Project_01_KNN/R/Librarys.R
@@ -0,0 +1,12 @@
+install.packages("rbenchmark")
+install.packages("Rcpp")
+install.packages("ElemStatLearn")
+
+library(ElemStatLearn)
+library(Rcpp)
+library(rbenchmark)
+
+
+
+install.packages("prim")
+library(Prim)
