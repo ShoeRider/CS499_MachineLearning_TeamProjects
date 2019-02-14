b8250c3167bc13a20aa22d8bf007cfd3c57fad03
Andrea Caviglia <acavi924@gmail.com>
2019-02-14 14:34:25 -0700

    fixing package check errors

--- Project_01_KNN/src/interface.cpp
+++ Project_01_KNN/src/interface.cpp
@@ -1,38 +1,37 @@
 #include "knn.h"
-#include <R.h>
-#include <R_ext/Rdynload.h>
+#include <R.h> // for error
+#include <R_ext/Rdynload.h> // for registration
 
 void knn_interface(
-    const double *train_input_ptr, // n_observations x n_features
-    const double *train_label_ptr, // n_observations
-    const double *test_input_ptr, // n_features
-    const int *n_observations_ptr,
-    const int *n_features_ptr,
-    const int *max_neighbors_ptr,
-    double *test_prediction_ptr // max neighbors
+    const double * train_input_ptr, // n_observations x n_features
+    const double * train_label_ptr, // n_observations
+    const double * test_input_ptr, // n_features
+    const int * n_observations_ptr,
+    const int * n_features_ptr,
+    const int * max_neighbors_ptr,
+    double * test_predict_ptr // max_neighbors
 )
 {
-  int status = knn(train_input_ptr,train_label_ptr,test_input_ptr,
-                   *n_observations_ptr, *n_features_ptr, *max_neighbors_ptr,
-                   test_prediction_ptr);
+  int status = knn(train_input_ptr, train_label_ptr,
+                   test_input_ptr, *n_observations_ptr,
+                   *n_features_ptr, *max_neighbors_ptr,
+                   test_predict_ptr);
   
-  if( status != 0 )
+  // change error messages
+  if (status != 0)
   {
-    error("Non-zero Exit Status from KNN");
+    error("non-zero exit status from knn");
   }
-
 }
 
-R_CMethodDef cMethods[]= {
-  {"knn_interface",(DL_FUNC) &knn_interface, 7 },
-  { NULL, NULL, 0 }
+R_CMethodDef cMethods[] = {
+  {"knn_interface", (DL_FUNC) &knn_interface, 7},
+  {NULL, NULL, 0}
 };
 
 extern "C" {
-  void R_init_nearestNeighborsAlg(DllInfo *info){
-    R_registerRoutines( info, cMethods, NULL, NULL, NULL);
-    R_useDynamicSymbols( info, FALSE);
+  void R_init_NearestNeighbors(DllInfo *info){
+    R_registerRoutines(info, cMethods, NULL, NULL, NULL);
+    R_useDynamicSymbols(info, FALSE);
   }
-
-
 }
