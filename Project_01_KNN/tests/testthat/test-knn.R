d2fcaa4b083814e192b75c784d4298224eba0072
Chadd Frasier <33643771+ChaddFrasier@users.noreply.github.com>
2019-02-14 09:59:36 -0700

    Update test-knn.R

--- Project_01_KNN/tests/testthat/test-knn.R
+++ Project_01_KNN/tests/testthat/test-knn.R
@@ -11,7 +11,7 @@ test_that("knn computes same answer as R", {
   y <- zip.train[train.i, 1]
   testx <- zip.train[test.i, -1]
   max.neighbors <- 4
-  pred.vec <- knn(x,y,testx,max.neighbors)
+  pred.vec <- knn(x,y,testx,max.neighbors) # try adding a brand new vector to the function 'double(max.neighbors)'
   dist.mat <- t(x) - testx
   dist.vec <- sqrt( colSums(dist.mat * dist.mat))
   sorted.index.vec <- order(dist.vec)
