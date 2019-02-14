3cb99350d8f25882e67fe654df66a11ace12822d
Andrea Caviglia <acavi924@gmail.com>
2019-02-14 15:28:27 -0700

    NN1toKmaxPredict C++ function and interface

--- Project_01_KNN/src/interface.cpp
+++ Project_01_KNN/src/interface.cpp
@@ -17,15 +17,85 @@ void knn_interface(
                    *n_features_ptr, *max_neighbors_ptr,
                    test_predict_ptr);
   
-  // change error messages
+  // error messages
   if (status != 0)
   {
-    error("non-zero exit status from knn");
+    if (status == NO_TRAIN_DATA_ERR)
+    {
+      error("no train data");
+    }
+    
+    else if (status == KMAX_TOO_SMALL_ERR)
+    {
+      error("too many neighbors");
+    }
+    
+    else if (status == KMAX_TOO_LARGE_ERR)
+    {
+      error("too few neighbors");
+    }
+    
+    else
+    {
+      error("unknown non-zero exit status from knn", status);
+    }
+  }
+}
+
+void NN1toKmaxPredict_interface
+  (const int * n_train_observations_ptr,
+   const int * n_test_observations_ptr,
+   const int * n_features_ptr,
+   const int * max_neighbors_ptr,
+   double * train_input_ptr, // n_train_observations x n_features
+   double * train_label_ptr, // n_train_observations
+   double * test_input_ptr, // n_test_observations x n_features
+   double * test_predict_ptr // n_test_observations x max_neighbors
+  )
+{
+  int status = NN1toKmaxPredict(*n_train_observations_ptr,
+                                *n_test_observations_ptr,
+                                *n_features_ptr,
+                                *max_neighbors_ptr,
+                                train_input_ptr,
+                                train_label_ptr,
+                                test_input_ptr,
+                                test_predict_ptr);
+  
+  // error messages
+  if (status != 0)
+  {
+    if (status == NO_TRAIN_DATA_ERR)
+    {
+      error("no train data");
+    }
+    
+    else if (status == NO_TEST_DATA_ERR)
+    {
+      error("no test data");
+    }
+    
+    else if (status == KMAX_TOO_SMALL_ERR)
+    {
+      error("too many neighbors");
+    }
+    
+    else if (status == KMAX_TOO_LARGE_ERR)
+    {
+      error("too few neighbors");
+    }
+    
+    else
+    {
+      error("unknown non-zero exit status from knn", status);
+    }
   }
 }
 
 R_CMethodDef cMethods[] = {
   {"knn_interface", (DL_FUNC) &knn_interface, 7},
+  {"NN1toKmaxPredict_interface",
+   (DL_FUNC) &NN1toKmaxPredict_interface, 8},
   {NULL, NULL, 0}
 };
 
--- Project_01_KNN/src/knn.cpp
+++ Project_01_KNN/src/knn.cpp
@@ -1,44 +1,131 @@
 #include <Eigen/Dense>
+#include "knn.h"
 
-int knn( 
-    const double *train_input_ptr, // n_observations x n_features
-    const double *train_label_ptr, // n_observations
-    const double *test_input_ptr, // n_features
-    const int n_observations, const int n_features, const int max_neighbors,
-    double *test_prediction_ptr // max neighbors
+int knn(
+    const double * train_input_ptr, // n_observations x n_features
+    const double * train_label_ptr, // n_observations
+    const double * test_input_ptr, // n_features
+    const int n_observations,
+    const int n_features,
+    const int max_neighbors,
+    double * test_predict_ptr // max_neighbors
 )
 {
-  Eigen::Map< Eigen::MatrixXd > train_input_mat(
-      (double*)train_input_ptr,n_observations,n_features);
-  Eigen::Map< Eigen::VectorXd > test_input_vec(
-      (double*)test_input_ptr,n_features);
+  // error checking
   
-  Eigen::VectorXd dist_vec(n_observations);
+  // validate positive train data dimensions
+  if (n_observations < 1)
+  {
+    return NO_TRAIN_DATA_ERR;
+  }
+  
+  // validate kmax is positive
+  if (max_neighbors < 1)
+  {
+    return KMAX_TOO_SMALL_ERR;
+  }
+  
+  // validate kmax < nrow
+  if (max_neighbors >= n_observations)
+  {
+    return KMAX_TOO_LARGE_ERR;
+  }
+  
+  Eigen::VectorXd distance_vec(n_observations);
+  Eigen::Map<Eigen::MatrixXd> train_inputs_mat(
+      (double *) train_input_ptr, n_observations, n_features);
+  Eigen::Map<Eigen::VectorXd> test_input_vec(
+      (double *) test_input_ptr, n_features);
   Eigen::VectorXi sorted_index_vec(n_observations);
   
-  for( int i=0; i<n_observations; i++)
+  for (int i = 0; i < n_observations; i++)
   {
-    dist_vec(i) = (
-      train_input_mat.row(i).transpose() - test_input_vec
-    ).norm();
+    distance_vec(i) = (
+      train_inputs_mat.row(i).transpose() - test_input_vec
+    ).lpNorm<1>(); // manhattan distance?
     sorted_index_vec(i) = i;
   }
+  
   std::sort(
     sorted_index_vec.data(),
     sorted_index_vec.data() + sorted_index_vec.size(),
-    [&dist_vec](int left, int right){
-      return dist_vec(left) < dist_vec(right);
+    [&distance_vec](int left, int right) {
+      return distance_vec(left) < distance_vec(right);
     }
   );
   
-  double total = 0.0;
-  for( int model_i = 0; model_i < max_neighbors; model_i++)
+  int row; int neighbors; double total_labels = 0.0;
+  
+  for (int k = 0; k < max_neighbors; k++)
   {
-    int neighbors = model_i + 1;
-    int row_i = sorted_index_vec(model_i);
-    total += train_label_ptr[row_i];
-    test_prediction_ptr[model_i] = total/neighbors;
+    row = sorted_index_vec(k);
+    neighbors = k + 1;
+    total_labels += train_label_ptr[row];
+    test_predict_ptr[k] = total_labels/neighbors;
   }
+  
   return 0;
-
 }
+
+
+int NN1toKmaxPredict(const int n_train_observations,
+                 const int n_test_observations,
+                 const int n_features,
+                 const int max_neighbors,
+                 double * train_input_ptr, // n_train_observations x n_features
+                 double * train_label_ptr, // n_train_observations
+                 double * test_input_ptr, // n_test_observations x n_features
+                 double * test_predict_ptr) // n_test_observations x max_neighbors
+{
+  
+  // error checking
+  
+  // validate positive train data dimensions
+  if (n_train_observations < 1)
+  {
+    return NO_TRAIN_DATA_ERR;
+  }
+  
+  // validate positive train data dimensions
+  if (n_test_observations < 1)
+  {
+    return NO_TEST_DATA_ERR;
+  }
+  
+  // validate kmax is positive
+  if (max_neighbors < 1)
+  {
+    return KMAX_TOO_SMALL_ERR;
+  }
+  
+  // validate kmax < nrow
+  if (max_neighbors >= n_train_observations)
+  {
+    return KMAX_TOO_LARGE_ERR;
+  }
+  
+  Eigen::Map<Eigen::MatrixXd> test_input_mat(
+      test_input_ptr,n_features, n_test_observations);
+  Eigen::Map<Eigen::MatrixXd> test_predict_mat(
+    test_predict_ptr, max_neighbors, n_test_observations);
+  
+  for (int i = 0; i < n_test_observations; i++)
+  {
+    knn(train_input_ptr, train_label_ptr,
+        test_input_mat.col(i).data(),
+        n_train_observations, n_features, max_neighbors,
+        test_predict_mat.col(i).data());
+  }
+  
+  return 0;
+}
+        
+  
+  
+  
+  
+  
+  
+  
+  
+  
