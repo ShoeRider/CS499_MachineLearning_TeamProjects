a008eb631bca085bd863fd7e15d6f3c48ec7b14e
Anthony Schroeder <36310164+ShoeRider@users.noreply.github.com>
2019-03-22 16:55:38 -0700

    Delete LMSquare_Functions_Interface.R

--- Project_02_LinearModel/R/LMSquare_Functions_Interface.R
+++
@@ -1,5 +0,0 @@
-#write at least two tests for each R function, in tests/testthat/test-xxx.R. You should at least test that (1) for valid
-#inputs your function returns an output of the expected type/dimension, (2) for an invalid input, your function stops
-#with an informative error message.
-
-
