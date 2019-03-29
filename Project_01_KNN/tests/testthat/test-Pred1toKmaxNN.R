76701cd30d4f3cc691d2ad0d82651edc448ceb3a
Chadd Frasier <33643771+ChaddFrasier@users.noreply.github.com>
2019-03-28 19:04:22 -0700

    Update test-Pred1toKmaxNN.R

--- Project_01_KNN/tests/testthat/test-Pred1toKmaxNN.R
+++ Project_01_KNN/tests/testthat/test-Pred1toKmaxNN.R
@@ -1,10 +1,11 @@
 library(NearestNeighbors)
 library(testthat)
+library(ElemStatLearn)
 context("NN1toKmaxPredict")
 
 test_that(
   "NN1toKmaxPredict checks for wrong input sizes",{
-    data(zip.train, packages= "ElemStatLearn")
+    data(zip.train, package= "ElemStatLearn")
     i01 <- which(zip.train[,1] %in% c(0,1))
     train.i <- i01[1:5]
     test.i <- i01[6]
@@ -18,7 +19,7 @@ test_that(
 
 test_that(
   "NN1toKmaxPredict checks for wrong input sizes",{
-    data(zip.train, packages= "ElemStatLearn")
+    data(zip.train, package= "ElemStatLearn")
     i01 <- which(zip.train[,1] %in% c(0,1))
     train.i <- i01[1:5]
     test.i <- i01[6]
@@ -28,4 +29,20 @@ test_that(
     max.neighbors <- 4
     pred.vec <- NN1toKmaxPredict(x,y,testx,max.neighbors)
     expect_failure('The function Failed as Planned')
+  })
+
+
+test_that(
+  "NN1toKmaxPredict checks for wrong input sizes",{
+    data(zip.train, package= "ElemStatLearn")
+    i01 <- which(zip.train[,1] %in% c(0,1))
+    train.i <- i01[1:5]
+    test.i <- i01[6]
+    x <- zip.train[train.i,-1]
+    y <- zip.train[train.i, 1]
+    testx <- zip.train[test.i, -1]
+    max.neighbors <- 4
+    pred.vec <- NN1toKmaxPredict(x,y,testx,max.neighbors)
+    
+    expect_equal(nrow(as.matrix(pred.vec)),1)
   })
