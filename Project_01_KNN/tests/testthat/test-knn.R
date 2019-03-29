f33b2042de895488096752a376a210b2c0945463
Chadd Frasier <33643771+ChaddFrasier@users.noreply.github.com>
2019-03-28 19:22:57 -0700

    Update test-knn.R

--- Project_01_KNN/tests/testthat/test-knn.R
+++ Project_01_KNN/tests/testthat/test-knn.R
@@ -4,7 +4,7 @@ library(testthat)
 context("knn")
 
 test_that("knn computes same answer as R", {
-  data(zip.train, packages= "ElemStatLearn")
+  data(zip.train, package= "ElemStatLearn")
   i01 <- which(zip.train[,1] %in% c(0,1))
   train.i <- i01[1:5]
   test.i <- i01[6]
@@ -19,4 +19,31 @@ test_that("knn computes same answer as R", {
   closest.indices <- sorted.index.vec[1:max.neighbors]
   expected.prediction <- cumsum(y[closest.indices])/(1:max.neighbors)
   expect_equal(pred.vec,expected.prediction)
+})
+
+test_that("knn checks for nulls data", {
+  data(zip.train, package= "ElemStatLearn")
+  i01 <- which(zip.train[,1] %in% c(0,1))
+  train.i <- i01[1:5]
+  test.i <- i01[6]
+  x <- zip.train[train.i,-1]
+  y <- zip.train[train.i, 1]
+  testx <- zip.train[test.i, -1]
+  max.neighbors <- 4
+  pred.vec <- knn(NULL,y,testx,max.neighbors)
+  
+  expect_failure('Test Failed Successfully ( as planned )')
+})
+
+test_that("knn checks for wrong sized data", {
+  data(zip.train, package= "ElemStatLearn")
+  i01 <- which(zip.train[,1] %in% c(0,1))
+  train.i <- i01[1:5]
+  test.i <- i01[6]
+  x <- zip.train[train.i,-1]
+  y <- zip.train[train.i, 1]
+  testx <- zip.train[test.i, -1]
+  max.neighbors <- 4
+  pred.vec <- knn(x[1:4,],y,testx,max.neighbors)
+  expect_failure('Failed Correctly')
 })
