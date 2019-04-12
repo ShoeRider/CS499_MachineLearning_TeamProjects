70da4968ea40c3e7ce191a05a68d61b4ab642cc1
Chadd Frasier <33643771+ChaddFrasier@users.noreply.github.com>
2019-04-12 16:21:12 -0700

    Update test-NNetEarlyStoppingCV.R

--- NeuralNetwork1/tests/testthat/test-NNetEarlyStoppingCV.R
+++ NeuralNetwork1/tests/testthat/test-NNetEarlyStoppingCV.R
@@ -10,19 +10,19 @@ test_that("NNetEarlyStoppingCV test size of input: clean run",{
   step.size <- .1
   n.folds <- 4
   Labels <- ozone[,1] # first col is output of the data
-  show(Labels)
+  #show(Labels)
   Data <- ozone[,2:ncol(ozone)]
-  
+
   TrainingData <- as.matrix(Data)[1:57,]
   TestData <- as.matrix(Data)[58:ncol(TrainingData),]
-  
+
   TrainingLabels <- Labels[1:57]
   TestLabels <- Labels[58:length(Labels)]
   is.train.vec <- sample(c(TRUE,FALSE),replace = TRUE, size = length(TrainingLabels))
   fold.vec <- sample(rep(1:n.folds), length(TrainingLabels),replace = TRUE)
-  
+
   output <- NNetEarlyStoppingCV(TrainingData,TrainingLabels, fold.vec, iterations,step.size,n.hidden.units,n.folds)
-  show(output)
+  #show(output)
   succeed()
 })
 
@@ -34,17 +34,17 @@ test_that("NNetEarlyStoppingCV test iteration type",{
   step.size <- .1
   n.folds <- 4
   Labels <- ozone[,1] # first col is output of the data
-  show(Labels)
+  #show(Labels)
   Data <- ozone[,2:ncol(ozone)]
-  
+
   TrainingData <- as.matrix(Data)[1:57,]
   TestData <- as.matrix(Data)[58:ncol(TrainingData),]
-  
+
   TrainingLabels <- Labels[1:57]
   TestLabels <- Labels[58:length(Labels)]
   is.train.vec <- sample(c(TRUE,FALSE),replace = TRUE, size = length(TrainingLabels))
   fold.vec <- sample(rep(1:n.folds), length(TrainingLabels),replace = TRUE)
-  
+
   # iteration is not an integer
   expect_error(NNetEarlyStoppingCV(TrainingData,TrainingLabels, fold.vec, iterations,step.size,n.hidden.units,n.folds))
 })
@@ -57,17 +57,64 @@ test_that("NNetEarlyStoppingCV test iteration type",{
   step.size <- .1
   n.folds <- 4
   Labels <- ozone[,1] # first col is output of the data
-  show(Labels)
+  # show(Labels)
   Data <- ozone[,2:ncol(ozone)]
-  
+
   TrainingData <- as.matrix(Data)[1:57,]
   TestData <- as.matrix(Data)[58:ncol(TrainingData),]
-  
+
+  TrainingLabels <- Labels[1:57]
+  TestLabels <- Labels[58:length(Labels)]
+
+  fold.vec <- sample(rep(1:n.folds), length(TrainingData),replace = TRUE)
+
+  # iteration is not an integer
+  expect_error(NNetEarlyStoppingCV(TrainingData,TrainingLabels, fold.vec, iterations,step.size,n.hidden.units,n.folds))
+})
+
+
+test_that("NNetEarlyStoppingCV test fold vector size validation",{
+  data(ozone, package = "ElemStatLearn")
+  # show(ozone)
+  iterations <- as.integer(20)
+  n.hidden.units <- 5
+  step.size <- .1
+  n.folds <- 4
+  Labels <- ozone[,1] # first col is output of the data
+  #show(Labels)
+  Data <- ozone[,2:ncol(ozone)]
+
+  TrainingData <- as.matrix(Data)[1:57,]
+  TestData <- as.matrix(Data)[58:ncol(TrainingData),]
+
   TrainingLabels <- Labels[1:57]
   TestLabels <- Labels[58:length(Labels)]
   is.train.vec <- sample(c(TRUE,FALSE),replace = TRUE, size = length(TrainingLabels))
-  fold.vec <- sample(rep(1:n.folds), length(TrainingData),replace = TRUE)
-  
+  fold.vec <- sample(rep(1:n.folds), length(TrainingData - 2),replace = TRUE)
+
   # iteration is not an integer
   expect_error(NNetEarlyStoppingCV(TrainingData,TrainingLabels, fold.vec, iterations,step.size,n.hidden.units,n.folds))
+})
+
+test_that("NNetEarlyStoppingCV test output vector size",{
+  data(ozone, package = "ElemStatLearn")
+  # show(ozone)
+  iterations <- as.integer(20)
+  n.hidden.units <- 5
+  step.size <- .1
+  n.folds <- 4
+  Labels <- ozone[,1] # first col is output of the data
+  #show(Labels)
+  Data <- ozone[,2:ncol(ozone)]
+
+
+  fold.vec <- sample(rep(1:n.folds), length(Data),replace = TRUE)
+
+  # iteration is not an integer
+  Data <- as.matrix(as.data.frame(lapply(Data, as.numeric)))
+  is.numeric(Data)
+
+  output <- NNetEarlyStoppingCV(Data, Labels, fold.vec, iterations,step.size,n.hidden.units,n.folds)
+
+  expect_equal(as.integer(length(output$mean.train.loss.vec)),as.integer(length(output$mean.validation.loss.vec)))
 })
