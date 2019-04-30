b0c9691d93ba6f583895ad73c99655c73b64e28b
ChaddFrasier <cmf339@nau.edu>
2019-04-30 13:50:51 -0700

    fixed building issues with DESCRIPTION and a parsing error in LinearModelL1. Also made skeleton for test

--- linearmodelL1_test/tests/testthat/test-LinearModelL1.R
+++ linearmodelL1_test/tests/testthat/test-LinearModelL1.R
@@ -1,1 +1,25 @@
-testaaaaaaaaaaaaaaaaaaaaa
+library(L1LinearModel)
+library(testthat)
+context("../R/linearmodelL1_test")
+
+test_that("NNetIterations test size of input: clean run",{
+  data(ozone, package = "ElemStatLearn")
+  # show(ozone)
+  iterations <- as.integer(20)
+  n.hidden.units <- 5
+  step.size <- .1
+  Labels <- ozone[,1] # first col is output of the data
+  show(Labels)
+  Data <- ozone[,2:ncol(ozone)]
+
+  TrainingData <- as.matrix(Data)[1:57,]
+  TestData <- as.matrix(Data)[58:ncol(TrainingData),]
+
+  TrainingLabels <- Labels[1:57]
+  TestLabels <- Labels[58:length(Labels)]
+  is.train.vec <- sample(c(TRUE,FALSE),replace = TRUE, size = length(TrainingLabels))
+
+  output <- NNetIterations(TrainingData,TrainingLabels,iterations,step.size,n.hidden.units,is.train.vec)
+  show(output)
+  succeed()
+})
